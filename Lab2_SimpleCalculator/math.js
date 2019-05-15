2345ba8968b3cf0193d097b00fbe2655ca0d07c5
Sidebail <tfg.devteam@gmail.com>
2019-05-15 12:01:39 -0400

    Simple Calculator modified and commented

--- Lab2_SimpleCalculator/math.js
+++ Lab2_SimpleCalculator/math.js
@@ -1,3 +1,18 @@
+/**
+ * This metgod handles the calculation.
+ * Using the switch it takes the method string parameter, which was given 
+ * in URL and determines the right method to execute
+ * 
+ * It can get either string or symbol, like "add" and "+"
+ * 
+ * In return it outputs the object, which has result number value and 
+ * operator string value.
+ * 
+ * UNIQUE ADDED - power, modulus
+ * @param {string} method 
+ * @param {number} x 
+ * @param {number} y 
+ */
 const handleCalculation = (method, x, y)=>{
     switch (method.toLowerCase()){
         case 'add':
@@ -12,16 +27,31 @@ const handleCalculation = (method, x, y)=>{
         case 'divide':
         case '/':
             return {result: x / y, operator: '/'}
+        case 'modulus':
+        case '%':
+            return{result: x % y, operator: '%'}
+        case 'power':
+        case '^':
+            return{result: Math.pow(x,y), operator: '^'}
         default:
-            return ' This is an invalid op'
+            return 'This is an invalid op, please try again with valid input!'
     }
 }
 
-validOptions = ['add', '+','subtract','-', 'multiply', '*', 'divide', '/']
+//Declaring all possible methods
+validOptions = ['add', '+','subtract','-', 'multiply', '*', 'divide', '/','modulus','%', 'power', '^']
 
+/**
+ * This method gets triggered whenever URL is passed and called through server.js
+ * Method handles the input validation and output
+ * 
+ * @param {request} req 
+ * @param {responce} res 
+ */
 const handleHttpCalculation = (req,res) => {
     const query = req.query;
 
+    //Check if x and y are valid numbers
     if(isNaN(query.x) || isNaN(query.y)){
         return res.send('Both X and Y must be number!');
     }
@@ -29,6 +59,7 @@ const handleHttpCalculation = (req,res) => {
 
     const method = query.method;
 
+    //Check if method is valid string and list of valid options has it
     if(!method || !validOptions.includes(method.toLowerCase())){
         return res.send(`Method must be included from following: ${
             validOptions.join(
@@ -36,6 +67,8 @@ const handleHttpCalculation = (req,res) => {
             )
         }`);
     }
+
+    //output the operands, operator and result
     res.send(`${query.x} ${handleCalculation(method, Number(query.x), Number(query.y)).operator} ${query.y} = ${handleCalculation(method, Number(query.x), Number(query.y)).result}`);
 };
 
--- Lab2_SimpleCalculator/server.js
+++ Lab2_SimpleCalculator/server.js
@@ -1,8 +1,17 @@
+/**
+ * Lab 2 - Simple Calculator
+ * Vladimir Vatsuirn
+ * 200363172
+ */
+
 const express = require('express'); //require is express
 const app = express(); //init express
 const PORT = 3000; //port out application wil run on
 const handleHttpCalculation = require('./math');
 
+/**
+ * Here we are getting the information from the URL and passing it to handler method
+ */
 app.get('/', handleHttpCalculation);
 app.listen(PORT);
 console.log(`Your application is running on https://localhost:${PORT}`);
