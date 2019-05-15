e3b026ca362f5bf2492b9bd84e21a26c1bad036c
Sidebail <tfg.devteam@gmail.com>
2019-05-15 12:06:59 -0400

    Update server.js

--- Lab2_SimpleCalculator/server.js
+++ Lab2_SimpleCalculator/server.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 /**
  * Lab 2 - Simple Calculator
  * Vladimir Vatsuirn
@@ -16,5 +15,3 @@ const handleHttpCalculation = require('./math');
 app.get('/', handleHttpCalculation);
 app.listen(PORT);
 console.log(`Your application is running on https://localhost:${PORT}`);
-=======
->>>>>>> parent of 03822fb... Calculator is made :)
