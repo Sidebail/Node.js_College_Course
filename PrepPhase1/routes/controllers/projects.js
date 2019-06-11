dfb4d7fe9b46df457f7866a72563d1bde556f97e
Sidebail <tfg.devteam@gmail.com>
2019-06-11 18:47:25 -0400

    Pug template is updated with sample of Project

--- PrepPhase1/routes/controllers/projects.js
+++ PrepPhase1/routes/controllers/projects.js
@@ -1,7 +1,14 @@
 // Projects CRUD
 // Creating
 // Create a new Project
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
 
+const projectSchema = new Schema();
+
+const Project = mongoose.model('Project', projectSchema);
+
+module.exports = Project;
 // Reading
 // Find a project by it's ID
 // Find all projects
