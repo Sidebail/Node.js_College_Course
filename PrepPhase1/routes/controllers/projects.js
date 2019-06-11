8358a8e1ee4ef59d9435fb73530d42f2fa8d5be3
Sidebail <tfg.devteam@gmail.com>
2019-06-11 18:54:20 -0400

    File movement + models creation + FindAll and FindById Created

--- PrepPhase1/routes/controllers/projects.js
+++ PrepPhase1/routes/controllers/projects.js
@@ -1,35 +1,21 @@
 // Projects CRUD
 // Creating
 // Create a new Project
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
+const Project = require('../../models/project');
 
-const classSchema = new Sche({
-    teacher: String,
-    courseCode: String,
-    title: String
-});
-
-const projectSchema = new Schema({
-    name: {
-        type: String,
-        required: true,
-    },
-    grade: {
-        type: Number,
-        min: 0,
-        max: 100
-    },
-    class: classSchema
-});
-
-const Project = mongoose.model('Project', projectSchema);
-
-module.exports = Project;
 // Reading
 // Find a project by it's ID
+// /projects/:id
+const findProjectById = async(req,res) => {
+    const id = req.params.id;
+    const projects = await Project.findById(id);
+    res.json(projects);
+};
 // Find all projects
-
+const findAllProjects = async(req, res) => {
+    const projects = await Project.find();
+    res.json(projects);
+};
 // Updating
 // Update a project based on it's ID
 
