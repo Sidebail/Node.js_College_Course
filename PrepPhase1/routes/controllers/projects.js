747614346a1a33d2f8d7efe0fd4a52f4071074ec
Sidebail <tfg.devteam@gmail.com>
2019-06-11 18:30:34 -0400

    TODO for projects.js added, controllers/projects.js created

---
+++ PrepPhase1/routes/controllers/projects.js
@@ -0,0 +1,15 @@
+// Projects CRUD
+// Creating
+// Create a new Project
+
+// Reading
+// Find a project by it's ID
+// Find all projects
+
+// Updating
+// Update a project based on it's ID
+
+// Deleting
+// Delete a project based on it's ID
+
+
