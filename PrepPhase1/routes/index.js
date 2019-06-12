d6eaf682a387dfb7d33e9c900b2827f83bfd1afc
Sidebail <tfg.devteam@gmail.com>
2019-06-11 20:20:10 -0400

    Bugs are fixed

--- PrepPhase1/routes/index.js
+++ PrepPhase1/routes/index.js
@@ -7,19 +7,21 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-// List all Projects (GET)
-router.get('/projects', projects.findAllProjects);
-// List a specific Project (GET)
-router.get('/projects/:id', projects.findProjectById);
-
 // Render create form (GET)
 router.get('/projects/new', (req, res) => res.render('projects/create'));
 // Handle create form (POST)
 router.post('/projects/new', projects.createNewProject);
 // Render edit form (GET)
+router.get('/projects/:id/edit', projects.findProjectById('projects/edit'));
+
 // Handle edit form (POST)
 router.post('/projects/:id/edit', projects.updateProjectById);
 // Delete a Project (GET)
 router.get('/projects/:id/delete', projects.deleteProjectById);
+
+// List all Projects (GET)
+router.get('/projects', projects.findAllProjects);
+// List a specific Project (GET)
+router.get('/projects/:id', projects.findProjectById('projects/details'));
 
 module.exports = router;
